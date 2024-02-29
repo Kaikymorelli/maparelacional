3769b264c3d38b1d81c29d29916eacaba04169ab
Kaikymorelli <132359674+Kaikymorelli@users.noreply.github.com>
2024-02-29 16:35:28 -0300

    Add files via upload

---
+++ Projetomaparelacional/src/main/java/com/Projetomaparelacional/ProjetomaparelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.Projetomaparelacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomaparelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomaparelacionalApplication.class, args);
+	}
+
+}
---
+++ Projetomaparelacional/src/test/java/com/Projetomaparelacional/ProjetomaparelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projetomaparelacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomaparelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
